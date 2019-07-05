fe38539d28642b356adcbd333f6076e537f3cf88
Ambrish Sinha <asinha@dargal.com>
2019-07-05 13:18:53 -0700

    CHanges done to make it work with ES7

--- test/lib/transport.exs
+++ test/lib/transport.exs
@@ -1,7 +1,7 @@
 defmodule ElasticClientTest.Transport do
   use ExUnit.Case
 
-  @test_config %{host: "dqa-es1.perx.com", port: 9200, index_prefix: "es_test"}
+  @test_config %{host: "dev-es1.perx.com", port: 9200, index_prefix: "es_test"}
 
   test :index_exists do
     
